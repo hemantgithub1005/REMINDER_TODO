6213ff95855dab7a114cd9b9e0a3e5c9b4716323
hemantgithub1005 <89726860+hemantgithub1005@users.noreply.github.com>
2021-10-06 03:09:07 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# My Reminders Web App
+
+A simple reminders app, built using TypeScipt, React and React-Redux. Reminders are saved to Local Storage.
+
+<p align="center">
+  <img src="./src/gifs/example.gif" width='450'/>
+</p>
+
+Hosted at [Netlify](https://reminders-app.netlify.app/).
+
+## Instructions
+
+1. Clone the repo or download all the files
+2. Install all the dependencies with `yarn install`
+3. Start the development server with `yarn start`
+
+## Authors
+
+- Ivan Cheng
---
+++ src/App.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { connect } from "react-redux";
+import { rootState } from "./store/reducers";
+import "./App.css";
+import { remindersState } from "./store/reducers/remindersReducer";
+import { ReminderActionTypes } from "./store/actions/remindersActions";
+import { ThunkDispatch } from "redux-thunk";
+import { listState } from "./store/reducers/listReducer";
+import RemindersHolder from "./components/RemindersHolder";
+import ListsHolder from "./components/ListsHolder";
+
+interface IProps {}
+
+interface IState {
+  currentList: string;
+}
+
+type Props = IProps & LinkStateProps & LinkDispatchProps;
+
+class App extends React.Component<Props, IState> {
+  state: IState = {
+    currentList: "",
+  };
+
+  componentDidMount() {
+    let defaultList = "";
+    if (this.props.lists.lists.length > 0) {
+      defaultList = this.props.lists.lists[0].name;
+    }
+
+    this.setState({
+      currentList: defaultList,
+    });
+  }
+
+  updateCurrentList = (newList?: string): void => {
+    const { currentList } = this.state;
+    const { lists } = this.props;
+
+    // for ListInput and List
+    if (newList) {
+      this.setState({
+        currentList: newList,
+      });
+    } else {
+      const defaultList = lists.lists.filter((l) => l.name !== currentList);
+      if (defaultList[0]) {
+        this.setState({
+          currentList: defaultList[0].name,
+        });
+      } else {
+        this.setState({
+          currentList: "",
+        });
+      }
+    }
+  };
+
+  render() {
+    const { currentList } = this.state;
+    return (
+      <div className="App">
+        <h1>Reminders</h1>
+        <div className="main-holder">
+          <ListsHolder
+            currentList={currentList}
+            updateCurrentList={this.updateCurrentList}
+          />
+          <RemindersHolder currentList={currentList} />
+        </div>
+      </div>
+    );
+  }
+}
+
+interface LinkStateProps {
+  reminders: remindersState;
+  lists: listState;
+}
+
+interface LinkDispatchProps {}
+
+const mapStateToProps = (
+  state: rootState,
+  ownProps: IProps
+): LinkStateProps => ({
+  reminders: state.reminders,
+  lists: state.lists,
+});
+
+const mapDispatchToProps = (
+  dispatch: ThunkDispatch<any, any, ReminderActionTypes>,
+  ownProps: IProps
+): LinkDispatchProps => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
